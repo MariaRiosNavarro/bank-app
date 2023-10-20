5b21cb70ff9096f9719ea9c5523ca02c5700a363
SuperCoder <propscode254@gmail.com>
2023-10-20 12:04:06 +0200

    add proptypes

--- src/components/NavigationButton/NavigationButton.jsx
+++ src/components/NavigationButton/NavigationButton.jsx
@@ -1,13 +1,19 @@
 import "./NavigationButton.scss";
+import PropTypes from "prop-types";
 
-const NavigationButton = (props) => {
+const NavigationButton = ({ name, handleClick }) => {
   return (
     <>
       <div className="nav-btn-wrapper">
-        <button onClick={props}>{props}</button>
+        <button onClick={handleClick}>{name}</button>
       </div>
     </>
   );
 };
 
 export default NavigationButton;
+
+NavigationButton.propTypes = {
+  name: PropTypes.string,
+  handleClick: PropTypes.func,
+};
