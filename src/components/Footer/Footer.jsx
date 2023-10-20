bc1292f5c1257b533e2108530f49561747a427e6
SuperCoder <propscode254@gmail.com>
2023-10-20 17:15:30 +0200

    add prototyps

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,26 @@
 import NavigationButton from "../NavigationButton/NavigationButton";
 import { homeSvg, addSvg, removeSvg, userSvg } from "../svg/svg";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./Footer.scss";
 
-const Footer = () => {
+const Footer = ({ onClick }) => {
   return (
     <div className="footer-wrapper">
       <Link to="/overview">
-        <NavigationButton svgIcon={homeSvg} />
+        <NavigationButton svgIcon={homeSvg} onClick={onClick} />
       </Link>
-      <NavigationButton svgIcon={addSvg} />
-      <NavigationButton svgIcon={removeSvg} />
+      <NavigationButton svgIcon={addSvg} onClick={onClick} />
+      <NavigationButton svgIcon={removeSvg} onClick={onClick} />
       <Link to="/user">
-        <NavigationButton svgIcon={userSvg} />
+        <NavigationButton svgIcon={userSvg} onClick={onClick} />
       </Link>
     </div>
   );
 };
 
 export default Footer;
+
+Footer.propTypes = {
+  onClick: PropTypes.func,
+};
