e60ffa5ffc0c54e1ef2fd94f15257bc61bc80554
SuperCoder <propscode254@gmail.com>
2023-10-22 14:06:57 +0200

    finisch

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,12 +1,32 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import { userSvg } from "../components/svg/svg";
+import "./user.scss";
 
 const User = () => {
   return (
     <>
-      <div className="wrapper">
+      <div className="wrapper user-main">
         <Header />
-        <h2 className="text-center">User</h2>
+        <div className="user-wrapper">
+          <div className="center">{userSvg}</div>
+          <div className="card-wrapper">
+            <div className="card">
+              <h2>Name: </h2>
+              <p>Jane</p>
+              <h2>Lastname:</h2>
+              <p>Doe</p>
+              <h3>Email</h3>
+              <p>jane@doe.es</p>
+            </div>
+          </div>
+          <div className="card-wrapper">
+            <div className="card">
+              <h2>Accounts: </h2>
+              <p>CreditCard</p>
+            </div>
+          </div>
+        </div>
         <Footer selected={"user"} />
       </div>
     </>
