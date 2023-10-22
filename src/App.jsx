76e1ce4d84a6f2bd9fc3fe7e4c6511288a0bb044
SuperCoder <propscode254@gmail.com>
2023-10-22 15:21:25 +0200

    change route for app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import User from "./pages/User";
 export const App = () => {
   return (
     <Routes>
-      <Route index element={<Login />} />
+      <Route path="/" index element={<Login />} />
       <Route path="/overview" element={<Overview />} />
       <Route path="/account" element={<Account />} />
       <Route path="/user" element={<User />} />
