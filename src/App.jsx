f4e4819aa687c1ea3bdcd2bf3486451804161430
SuperCoder <propscode254@gmail.com>
2023-10-27 09:29:47 +0200

    fix useContext & routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import AccountPage from "./pages/AccountPage";
 import UserPage from "./pages/UserPage";
 import OverviewPage from "./pages/OverviewPage";
+import { AppProvider } from "./AppContext";
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" index element={<LoginPage />} />
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<LoginPage />} />
 
-      <Route path="/OverviewPage" element={<OverviewPage />} />
+          <Route path="/OverviewPage" element={<OverviewPage />} />
 
-      <Route path="/AccountPage" element={<AccountPage />} />
+          <Route path="/AccountPage" element={<AccountPage />} />
 
-      <Route path="/UserPage" element={<UserPage />} />
+          <Route path="/UserPage" element={<UserPage />} />
 
-      {/* TODO: <Route path="*" element={<NotFound />} /> */}
-    </Routes>
+          {/* TODO: <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   );
 };
 
