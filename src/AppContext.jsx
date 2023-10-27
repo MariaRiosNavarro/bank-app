5f9d6d9c7e69f593c4f7131779985ac31cf4944d
SuperCoder <propscode254@gmail.com>
2023-10-27 10:14:02 +0200

    add coment

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,10 +1,11 @@
 import { createContext, useContext, useState } from "react";
 import PropTypes from "prop-types";
 
+// use it at the components
 const AppContext = createContext();
-
 export const useAppContext = () => useContext(AppContext);
 
+// use it at the main App
 export const AppProvider = ({ children }) => {
   const [totalCardAmount, setTotalCardAmount] = useState(0);
   const [history, setHistory] = useState([]);
